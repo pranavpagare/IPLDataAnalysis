85df299f631f6e99d1d6463802164c05bf03bcc0
Pranav Pagare <37401153+pranavpagare@users.noreply.github.com>
2020-05-21 01:13:18 +0530

    Create 18.sql
    Most sixex scored by a batsman against a bowler in a match.
    Highest is 6 sixes has occured 6 times.

---
+++ 18.sql
@@ -0,0 +1,5 @@
+Select d.match_id,d.batsman, d.bowler ,count(d.batsman_runs) as count FROM deliveries as d
+WHERE d.batsman_runs = 6 and (d.wide_runs=0 and d.bye_runs=0 and d.legbye_runs=0) 
+group by d.match_id,d.batsman , d.bowler
+order by count desc
+Limit 20
